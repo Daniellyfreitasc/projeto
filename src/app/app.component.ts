83aec1e86d8ec6317caf30b20bcba442dbd4cce0
Daniellyfreitasc <daniellyfreitasc@hotmail.com>
2021-11-17 19:21:13 -0300

    Curso MPro Consultoria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,52 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { DirectiveAst } from '@angular/compiler';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Dia } from './dia.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  public form: FormGroup = new FormGroup({
-    'Aberturasegunda': new FormControl('', [Validators.required, Validators.minLength(4)]),
-    'Fechamentosegunda': new FormControl('', [Validators.minLength(4)]),
-    'Aberturaterca': new FormControl('', [Validators.minLength(4)]),
-    'Fechamentoterca': new FormControl('', [Validators.minLength(4)]),
-    'Aberturaquarta': new FormControl('', [Validators.minLength(4)]),
-    'Fechamentoquarta': new FormControl('', [Validators.minLength(4)]),
-    'Aberturaquinta': new FormControl('', [Validators.minLength(4)]),
-    'Fechamentoquinta': new FormControl('', [Validators.minLength(4)]),
-    'Aberturasexta': new FormControl('', [Validators.minLength(4)]),
-    'Fechamentosexta': new FormControl('', [Validators.minLength(4)]),
-    'Aberturasab': new FormControl('', [Validators.minLength(4)]),
-    'Fechamentosab': new FormControl('', [Validators.minLength(4)]),
-    'Aberturadom': new FormControl('', [Validators.minLength(4)]),
-    'Fechamentodom': new FormControl('', [Validators.minLength(4)])
-  })
-
-  constructor(private formBuilder: FormBuilder ) {
-    
-  }
-
- 
+export class AppComponent implements OnInit {
   
+  
+
+  panelOpenState = false;
+  selectDisabled = false;
+
+  dias: Dia[];
+
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder ) {
+    this.form = this.formBuilder.group({
+      abertura: [""],
+      fechamento: [""]
+   });
+
+   this.dias = [
+     {id:1, name: "Domingo"},
+     {id:2, name: "Segunda"},
+     {id:3, name: "Terça"},
+     {id:4, name: "Quarta"},
+     {id:5, name: "Quinta"},
+     {id:6, name: "Sexta"},
+     {id:7, name: "Sábado"}
+   ];
+  } 
+
+  ngOnInit(){
+
+
+  
+  
+  
+}
+
+cbChange(event: any){
+   console.log(event.checked);
+   this.selectDisabled = event.checked;
+     
+   } 
+     
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,39 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {  MatSliderModule } from '@angular/material/slider';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import {MatRadioModule} from '@angular/material/radio';
-
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DiaComponent } from './dia/dia.component';
+import {MatCardModule} from '@angular/material/card';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     ReactiveFormsModule,
     MatSliderModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatExpansionModule,
     MatCheckboxModule,
     MatRadioModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
